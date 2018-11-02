35e433e19099609debc382997b1763d08490220f
Zarko Stanisic <zarko.stanisic@live.com>
2018-11-02 11:29:58 +0100

    lesson 24.

--- laravel-qa/app/Answer.php
+++ laravel-qa/app/Answer.php
@@ -21,4 +21,17 @@ class Answer extends Model
     public function getBodyHtmlAttribute(){
         return \Parsedown::instance()->text($this->body);
     }
+
+    public static function boot(){
+    	parent::boot();
+
+    	static::created(function($answer){
+    		$answer->question->increment('answers_count');
+    		$answer->question->save();
+    	});
+
+    	// static::saved(function($answer){
+    	// 	echo 'Answer saved\n';
+    	// });
+    }
 }
