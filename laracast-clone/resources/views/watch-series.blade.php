8b6553395b398428bf5bfc24e44d6929c340c98d
Zarko Stanisic <zarko.stanisic@live.com>
2018-12-02 17:42:59 +0100

    72

--- laracast-clone/resources/views/watch-series.blade.php
+++ laracast-clone/resources/views/watch-series.blade.php
@@ -24,8 +24,31 @@
 <div class="container">
 
   <div class="row gap-y">
-    <div class="col-12 col-md-12">
+    <div class="col-12 col-md-12 text-center">
       <vue-player default_lesson="{{ $lesson }}"></vue-player>
+    </div>
+    <div class="col-md-12">
+    @if ($lesson->prevLesson())
+      <a href="{{ route('series.watch', [$series->slug, $lesson->prevLesson()->id]) }}" class="btn btn-info float:left">PREV LESSON</a>
+    @endif
+
+    @if ($lesson->nextLesson())
+      <a href="{{ route('series.watch', [$series->slug, $lesson->nextLesson()->id]) }}" class="btn btn-info float-right">NEXT LESSON</a>
+    @endif
+    </div>
+    <div class="col-md-12">
+      <ul class="list-group">
+        @foreach ($series->getOrderedLessons() as $l)
+          <li class="list-group-item d-flex justify-content-between">
+            {{ $l->title }}
+            @if ($lesson->id == $l->id)
+              <p>NOW PLAYING!</p>
+            @else
+              <a href="{{ route('series.watch', [$series->slug, $l->id]) }}" class="btn btn-primary">PLAY</a>
+            @endif
+          </li>
+        @endforeach
+      </ul>
     </div>
 </div>
 </div>
