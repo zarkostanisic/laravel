2573dd31fcf25695a3085c0b089292ba57ae354a
Zarko Stanisic <zarko.stanisic@live.com>
2018-12-10 22:17:34 +0100

    30

--- real-time-forum/app/Http/Controllers/AuthController.php
+++ real-time-forum/app/Http/Controllers/AuthController.php
@@ -14,8 +14,8 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        // $this->middleware('auth:api', ['except' => ['login']]);
-        $this->middleware('jwt', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login']]);
+        // $this->middleware('jwt', ['except' => ['login']]);
     }
 
     /**
