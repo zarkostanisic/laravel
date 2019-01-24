b01c2f80089864493f9cc80092aaf8a3af17ed7f
Zarko Stanisic <zarko.stanisic@live.com>
2019-01-24 11:10:49 +0100

    4.

--- eapi/database/factories/ProductFactory.php
+++ eapi/database/factories/ProductFactory.php
@@ -1,9 +1,14 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100, 2000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(10, 30)
     ];
 });
