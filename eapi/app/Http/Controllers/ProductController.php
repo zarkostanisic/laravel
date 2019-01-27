40fece4dca966deb43b7625b75cc019b7cc40c6e
Zarko Stanisic <zarko.stanisic@live.com>
2019-01-27 10:45:57 +0100

    9.

--- eapi/app/Http/Controllers/ProductController.php
+++ eapi/app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
+        $products = Product::paginate(5);
 
         return ProductCollection::collection($products);
     }
